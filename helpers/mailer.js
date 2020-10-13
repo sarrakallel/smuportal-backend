44650abdf2a56290118e8635eb39f5368e02f022
Mootez <mootez.saad@medtech.tn>
2020-10-13 16:35:06 +0100

    Regenerate oauth 2.0 access token when it is expired

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -1,45 +1,45 @@
-require("dotenv").config();
-const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
 const OAuth2 = google.auth.OAuth2;
 
 const emailMessages = (email, url) => {
   return {
-    confirmEmail: function() {
+    confirmEmail: function () {
       return {
         to: email,
-        subject: "Confirm Email",
-        html: `Please click this link to confirm your account <a href="${url}">${url}</a>`
+        subject: 'Confirm Email',
+        html: `Please click this link to confirm your account <a href="${url}">${url}</a>`,
       };
     },
-    resetPassword: function() {
+    resetPassword: function () {
       return {
         to: email,
-        subject: "Reset password",
-        html: `Please click this link to reset your account <a href="${url}">${url}</a>`
+        subject: 'Reset password',
+        html: `Please click this link to reset your account <a href="${url}">${url}</a>`,
       };
-    }
+    },
   };
 };
 
 const otpEmailMessages = (email, otp) => {
   return {
-    sendOtp: function() {
+    sendOtp: function () {
       return {
         to: email,
-        subject: "Two Factor Authorization Code",
-        html: `Code : ${otp}`
+        subject: 'Two Factor Authorization Code',
+        html: `Code : ${otp}`,
       };
-    }
+    },
   };
 };
 
 const boxBookingConfirmation = (user, email, box, date, start, end) => {
   return {
-    sendBoxConfirmation: function() {
+    sendBoxConfirmation: function () {
       return {
         to: email,
-        subject: "Box reservation",
+        subject: 'Box reservation',
         html: `<h3><strong>Dear ${user},</strong></h3> Box ${box} has been booked for you on the ${date} from ${start} until ${end}.
         <br>
         <br>Please make sure to keep the box clean after you leave.
@@ -49,9 +49,9 @@ const boxBookingConfirmation = (user, email, box, date, start, end) => {
         <br>
         <br>
         <hr>
-    <div style="color:rgb(127, 127, 127);font-size:small;text-align:center"><em>ISS396 Project, Booking System Application.</em></div>`
+    <div style="color:rgb(127, 127, 127);font-size:small;text-align:center"><em>ISS396 Project, Booking System Application.</em></div>`,
       };
-    }
+    },
   };
 };
 // Get OAuth2 "session" token
@@ -62,11 +62,11 @@ const oauth2Client = new OAuth2(
 );
 
 oauth2Client.setCredentials({
-  refresh_token: process.env.G_REFRESH_TOKEN
+  refresh_token: process.env.G_REFRESH_TOKEN,
 });
 
 // Refresh token might be broken! We need to fix it
-
+const accessToken = oauth2Client.getAccessToken();
 // NodeMailer transporter
 const transporter = nodemailer.createTransport({
   service: process.env.SMTP_SERVICE,
@@ -76,13 +76,13 @@ const transporter = nodemailer.createTransport({
     clientId: process.env.G_CLIENT_ID,
     clientSecret: process.env.G_CLIENT_SECRET,
     refreshToken: process.env.G_REFRESH_TOKEN,
-    accessToken: process.env.G_ACCESS_TOKEN
-  }
+    accessToken: accessToken,
+  },
 });
 
 module.exports = {
   emailMessages: emailMessages,
   otpEmailMessages: otpEmailMessages,
   transporter: transporter,
-  boxBookingConfirmation: boxBookingConfirmation
+  boxBookingConfirmation: boxBookingConfirmation,
 };
